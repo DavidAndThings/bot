25cc9fa5499bff836790c3897bdc43fa28251db7
Xiao Wang <davidandthings@hey.com>
2023-01-02 20:30:02 -0500

    added elimination function for there exists

--- fol.py
+++ fol.py
@@ -1,18 +1,19 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
+from typing import Mapping
 
 
 @dataclass
 class ThereExists:
     variables: tuple[str, ...]
-    children: tuple[Clause, ...]
+    clause: Clause
 
 
 @dataclass
 class ForAll:
     variables: tuple[str, ...]
-    children: tuple[Clause, ...]
+    clause: Clause
 
 
 @dataclass
@@ -30,8 +31,89 @@ class Implies:
 @dataclass
 class Predicate:
     name: str
-    arguments: tuple[str, ...]
+    arguments: tuple[str | SkolemFunction, ...]
+
+
+@dataclass
+class SkolemFunction:
+    variables: tuple[str, ...]
 
 
 Quantifier = ForAll | ThereExists
 Clause = And | Predicate | ForAll | ThereExists | Implies
+
+
+def eliminate_existantial(
+    clause: Clause,
+    universally_quantified_variables: tuple[str, ...],
+    variable_map: Mapping[str, tuple[str, ...]],
+) -> Clause:
+
+    if isinstance(clause, ThereExists):
+
+        new_variable_map = {**variable_map}
+
+        for v in clause.variables:
+            new_variable_map = {**new_variable_map, v: universally_quantified_variables}
+
+        return eliminate_existantial(
+            clause=clause.clause,
+            universally_quantified_variables=universally_quantified_variables,
+            variable_map=new_variable_map,
+        )
+
+    elif isinstance(clause, ForAll):
+
+        return ForAll(
+            variables=clause.variables,
+            clause=eliminate_existantial(
+                clause=clause.clause,
+                universally_quantified_variables=universally_quantified_variables
+                + clause.variables,
+                variable_map=variable_map,
+            ),
+        )
+
+    elif isinstance(clause, And):
+
+        return And(
+            head=eliminate_existantial(
+                clause=clause.head,
+                universally_quantified_variables=universally_quantified_variables,
+                variable_map=variable_map,
+            ),
+            tail=eliminate_existantial(
+                clause=clause.head,
+                universally_quantified_variables=universally_quantified_variables,
+                variable_map=variable_map,
+            ),
+        )
+
+    elif isinstance(clause, Implies):
+
+        return Implies(
+            head=eliminate_existantial(
+                clause=clause.head,
+                universally_quantified_variables=universally_quantified_variables,
+                variable_map=variable_map,
+            ),
+            tail=eliminate_existantial(
+                clause=clause.head,
+                universally_quantified_variables=universally_quantified_variables,
+                variable_map=variable_map,
+            ),
+        )
+
+    elif isinstance(clause, Predicate):
+
+        new_args: tuple[str | SkolemFunction, ...] = ()
+
+        for i in clause.arguments:
+
+            if isinstance(i, str) and i in variable_map:
+                new_args += (SkolemFunction(variables=variable_map[i]),)
+
+            else:
+                new_args += (i,)
+
+        return Predicate(name=clause.name, arguments=new_args)
